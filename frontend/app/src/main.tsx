dcdf52e07cc5805ef2005767e757b7773a412a76
DailyDisco <actual.mensch0r@icloud.com>
2025-03-03 11:19:15 -0600

    init layout for pre and post authed layout

--- frontend/app/src/main.tsx
+++ frontend/app/src/main.tsx
@@ -4,6 +4,9 @@ import "./styles/styles.scss"
 import App from "./App.tsx"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { BrowserRouter, Route, Routes } from "react-router";
+import PreAuthedLayout from "./providers/layout/PreAuthedLayout.tsx"
+import AuthenticatedLayout from "./providers/layout/AuthenticatedLayout.tsx"
+import Settings from "./components/Settings.tsx"
 
 
 const queryClient = new QueryClient()
@@ -29,10 +32,14 @@ createRoot(document.getElementById("root")!).render(
           */}
 
           {/* Main Route (Landing Page) */}
-          <Route path="/" element={<App />} />
+          {/* Unauthenticated Layout */}
+          <Route path="/" element={<PreAuthedLayout />} >
+            <Route path="/" element={<App />} />
+          </Route>
+
 
           {/* Authentication Route Group */}
-          <Route path="authentication">
+          <Route path="auth" element={<PreAuthedLayout />}>
             <Route path="login" element={<h1>Login</h1>} />
 
             {/* Not sure we need this one since we are having the admins create a tenant */}
@@ -40,21 +47,28 @@ createRoot(document.getElementById("root")!).render(
             <Route path="register" element={<h1>Register</h1>} />
           </Route>
 
-          {/* Admin Route Group */}
-          <Route path="admin">
-            <Route index element={<h1>Admin Dashboard</h1>} />
-            <Route path="init-apartment-complex" element={<h1>Initial Admin Apartment Complex Setup</h1>} />
-            <Route path="add-tenant" element={<h1>Add Tenant</h1>} />
-            <Route path="admin-view-and-edit-leases" element={<h1>Admin View & Edit Leases</h1>} />
-            <Route path="admin-view-and-edit-work-orders" element={<h1>Admin View & Edit Work Orders</h1>} />
-          </Route>
+          <Route element={<AuthenticatedLayout />}>
+            {/* Admin Route Group */}
+            <Route path="admin" >
+              <Route index element={<h1>Admin Dashboard</h1>} />
+              <Route path="init-apartment-complex" element={<h1>Initial Admin Apartment Complex Setup</h1>} />
+              <Route path="add-tenant" element={<h1>Add Tenant</h1>} />
+              <Route path="admin-view-and-edit-leases" element={<h1>Admin View & Edit Leases</h1>} />
+              <Route path="admin-view-and-edit-work-orders-and-complaints" element={<h1>Admin View & Edit Work Orders & Complaints</h1>} />
+            </Route>
 
-          {/* Tenant Route Group */}
-          <Route path="tenant">
-            <Route index element={<h1>Tenant Dashboard</h1>} />
-            <Route path="guest-parking" element={<h1>Guest Parking</h1>} />
-            <Route path="digital-documents" element={<h1>Digital Documents</h1>} />
-            <Route path="work-orders-and-complaints" element={<h1>Work Orders & Complaints</h1>} />
+            {/* Tenant Route Group */}
+            <Route path="tenant">
+              <Route index element={<h1>Tenant Dashboard</h1>} />
+              <Route path="guest-parking" element={<h1>Guest Parking</h1>} />
+              <Route path="tenant-view-and-edit-leases" element={<h1>Digital Documents</h1>} />
+              <Route path="tenant-work-orders-and-complaints" element={<h1>Work Orders & Complaints</h1>} />
+            </Route>
+
+            {/* Settings Route */}
+            {/* TODO: Make sure to move this to the AuthenticatedLayout and use specific settings for each user role */}
+            <Route path="components/settings" element={<Settings />} />
+
           </Route>
 
           {/* 404 Route - Always place at the end to catch unmatched routes */}
