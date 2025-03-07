f818921fa7d14aaacc538c444c16ca281b1e5122
James Musselman <jtmuss@pm.me>
2025-03-07 15:56:07 -0600

    Chore: Remove obsolete login button

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,10 +1,8 @@
-
 import HeroBanner from "./components/HeroBanner";
 import HomePageFAQs from "./components/HomePageFAQs";
-import HomePageFeaturesComponent from "./components/HomePageFeaturesComponent"
+import HomePageFeaturesComponent from "./components/HomePageFeaturesComponent";
 import ClerkAuthDemo from "./components/ClerkAuthDemo";
 import DemoTestingComponent from "./components/DemoTestingComponent";
-
 
 function App() {
   return (
@@ -12,13 +10,6 @@ function App() {
       <HeroBanner />
       <HomePageFeaturesComponent />
       <HomePageFAQs />
-
-
-
-        {/* Login Button */}
-        <Link to="/auth/login">
-          <Button className="my-2">Login</Button>
-        </Link>
 
       {/* Let's completely remove this component once we are done testing */}
       {/* Or when everyone is confident they understand the code and how to use it themselves moving forward */}
@@ -30,7 +21,5 @@ function App() {
     </>
   );
 }
-
-
 
 export default App;
