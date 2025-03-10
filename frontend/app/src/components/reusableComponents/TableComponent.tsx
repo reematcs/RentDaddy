65ab60e8e72494dd7e2856cc6e52844732766a44
DailyDisco <actual.mensch0r@icloud.com>
2025-03-10 10:48:38 -0500

    Init Admin Page Frontend

--- frontend/app/src/components/reusableComponents/TableComponent.tsx
+++ frontend/app/src/components/reusableComponents/TableComponent.tsx
@@ -1,18 +1,18 @@
-import React from "react";
 import { Table } from "antd";
 import type { TableColumnType, TableProps } from "antd";
 
 interface TableComponentProps {
     columns: TableColumnType[];
-    data: any[];
+    dataSource: any[];
     onChange: (pagination: any, filters: any, sorter: any, extra: any) => void;
+    icon?: any;
 }
 
 export const TableComponent = (props: TableComponentProps) => {
     return (
         <Table
             columns={props.columns}
-            dataSource={props.data}
+            dataSource={props.dataSource}
             onChange={props.onChange}
             className="table"
         />
