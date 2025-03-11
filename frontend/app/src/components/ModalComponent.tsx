680f1b54c4b5aa27c492a70e9f844fb2d4576f03
schraderbachar <jj.schraderbachar@icloud.com>
2025-03-11 18:09:55 -0500

    fixed css and added modal component stuff

--- frontend/app/src/components/ModalComponent.tsx
+++ frontend/app/src/components/ModalComponent.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
-import { Button, Divider, Form, Input, Modal } from 'antd';
+import { useState } from "react";
+import { Button, Divider, Form, Input, Modal } from "antd";
 
 interface ModalComponentProps {
     buttonTitle: string;
+    buttonType: "default" | "primary" | "secondary" | "accent" | "info" | "success" | "warning" | "danger";
     content: string;
     type: "default" | "Smart Locker" | "Guest Parking";
     handleOkay: () => void;
@@ -20,16 +21,18 @@ const ModalComponent = (props: ModalComponentProps) => {
     };
 
     const titles = {
-        "default": "Default Modal",
+        default: "Default Modal",
         "Smart Locker": "Smart Locker Modal",
-        "Guest Parking": "Register someone in Guest Parking"
-    }
+        "Guest Parking": "Register someone in Guest Parking",
+    };
 
     return (
         <>
             {props.type === "default" && (
                 <>
-                    <Button type="primary" onClick={showModal}>
+                    <Button
+                        type="primary"
+                        onClick={showModal}>
                         {props.buttonTitle}
                     </Button>
                     <Modal
@@ -38,118 +41,134 @@ const ModalComponent = (props: ModalComponentProps) => {
                         onOk={props.handleOkay}
                         onCancel={handleCancel}
                         okButtonProps={{ hidden: true, disabled: true }}
-                        cancelButtonProps={{ hidden: true, disabled: true }}
-                    >
+                        cancelButtonProps={{ hidden: true, disabled: true }}>
                         <p>{props.content}</p>
                         <div className="flex justify-content-end gap-2">
-                            <Button type="default" onClick={handleCancel}>
+                            <Button
+                                type="default"
+                                onClick={handleCancel}>
                                 Cancel
                             </Button>
                             {/* Change to ButtonComponent.tsx once JJ's merge is completed with the Config Provider and Components */}
-                            <Button type="primary" onClick={props.handleOkay}>
+                            <Button
+                                type="primary"
+                                onClick={props.handleOkay}>
                                 Confirm
                             </Button>
                         </div>
                     </Modal>
                 </>
-            )
-            }
-            {
-                props.type === "Smart Locker" && (
-                    <>
-                        <Button type="primary" onClick={showModal}>
-                            {props.buttonTitle}
-                        </Button>
-                        <Modal
-                            className='p-3 flex-wrap-row'
-                            title={titles[props.type]}
-                            open={isModalOpen}
-                            onOk={props.handleOkay}
-                            onCancel={handleCancel}
-                            okButtonProps={{ hidden: true, disabled: true }}
-                            cancelButtonProps={{ hidden: true, disabled: true }}
-                        >
-                            <Form>
-                                <Form.Item name="search">
-                                    <Input placeholder='Search for a Tenant' />
-                                </Form.Item>
-                                <Form.Item name="locker-number">
-                                    <Input placeholder='Locker Number' type='number' />
-                                </Form.Item>
-                                <Divider />
-                                <div className="flex justify-content-end gap-2">
-                                    {/* Cancel button */}
-                                    <Form.Item name="cancel">
-                                        <Button type="default" onClick={() => {
-                                            setIsModalOpen(false)
+            )}
+            {props.type === "Smart Locker" && (
+                <>
+                    <Button
+                        type="primary"
+                        onClick={showModal}>
+                        {props.buttonTitle}
+                    </Button>
+                    <Modal
+                        className="p-3 flex-wrap-row"
+                        title={titles[props.type]}
+                        open={isModalOpen}
+                        onOk={props.handleOkay}
+                        onCancel={handleCancel}
+                        okButtonProps={{ hidden: true, disabled: true }}
+                        cancelButtonProps={{ hidden: true, disabled: true }}>
+                        <Form>
+                            <Form.Item name="search">
+                                <Input placeholder="Search for a Tenant" />
+                            </Form.Item>
+                            <Form.Item name="locker-number">
+                                <Input
+                                    placeholder="Locker Number"
+                                    type="number"
+                                />
+                            </Form.Item>
+                            <Divider />
+                            <div className="flex justify-content-end gap-2">
+                                {/* Cancel button */}
+                                <Form.Item name="cancel">
+                                    <Button
+                                        type="default"
+                                        onClick={() => {
+                                            setIsModalOpen(false);
                                         }}>
-                                            Cancel
-                                        </Button>
-                                    </Form.Item>
-                                    <Form.Item name="submit">
-                                        <Button type="primary" htmlType="submit">
-                                            Submit
-                                        </Button>
-                                    </Form.Item>
-                                </div>
-                            </Form>
-                        </Modal>
-                    </>
-                )
-            }
-            {
-                props.type === "Guest Parking" && (
-                    <>
-                        <Button type="primary" onClick={showModal}>
-                            {props.buttonTitle}
-                        </Button>
-                        <Modal
-                            className='p-3 flex-wrap-row'
-                            title={titles[props.type]}
-                            open={isModalOpen}
-                            onOk={props.handleOkay}
-                            onCancel={handleCancel}
-                            okButtonProps={{ hidden: true, disabled: true }}
-                            cancelButtonProps={{ hidden: true, disabled: true }}
-                        >
-                            <Form>
-                                <Form.Item name="tenant-name">
-                                    <Input placeholder='Tenant Name' />
+                                        Cancel
+                                    </Button>
                                 </Form.Item>
-                                <Form.Item name="license-plate-number">
-                                    <Input placeholder='License Plate Number' />
+                                <Form.Item name="submit">
+                                    <Button
+                                        type="primary"
+                                        htmlType="submit">
+                                        Submit
+                                    </Button>
                                 </Form.Item>
-                                <Form.Item name="car-color">
-                                    <Input placeholder='Car Color' type='number' />
-                                </Form.Item>
-                                <Form.Item name="car-make">
-                                    <Input placeholder='Car Make' />
-                                </Form.Item>
-                                <Form.Item name="duration-of-stay">
-                                    <Input placeholder='Duration of Stay' type='number' />
-                                </Form.Item>
-                                <Divider />
-                                <div className="flex justify-content-end gap-2">
-                                    {/* Cancel button */}
-                                    <Form.Item name="cancel">
-                                        <Button type="default" onClick={() => {
-                                            setIsModalOpen(false)
+                            </div>
+                        </Form>
+                    </Modal>
+                </>
+            )}
+            {props.type === "Guest Parking" && (
+                <>
+                    <Button
+                        type="primary"
+                        onClick={showModal}>
+                        {props.buttonTitle}
+                    </Button>
+                    <Modal
+                        className="p-3 flex-wrap-row"
+                        title={titles[props.type]}
+                        open={isModalOpen}
+                        onOk={props.handleOkay}
+                        onCancel={handleCancel}
+                        okButtonProps={{ hidden: true, disabled: true }}
+                        cancelButtonProps={{ hidden: true, disabled: true }}>
+                        <Form>
+                            <Form.Item name="tenant-name">
+                                <Input placeholder="Tenant Name" />
+                            </Form.Item>
+                            <Form.Item name="license-plate-number">
+                                <Input placeholder="License Plate Number" />
+                            </Form.Item>
+                            <Form.Item name="car-color">
+                                <Input
+                                    placeholder="Car Color"
+                                    type="number"
+                                />
+                            </Form.Item>
+                            <Form.Item name="car-make">
+                                <Input placeholder="Car Make" />
+                            </Form.Item>
+                            <Form.Item name="duration-of-stay">
+                                <Input
+                                    placeholder="Duration of Stay"
+                                    type="number"
+                                />
+                            </Form.Item>
+                            <Divider />
+                            <div className="flex justify-content-end gap-2">
+                                {/* Cancel button */}
+                                <Form.Item name="cancel">
+                                    <Button
+                                        type="default"
+                                        onClick={() => {
+                                            setIsModalOpen(false);
                                         }}>
-                                            Cancel
-                                        </Button>
-                                    </Form.Item>
-                                    <Form.Item name="submit">
-                                        <Button type="primary" htmlType="submit">
-                                            Submit
-                                        </Button>
-                                    </Form.Item>
-                                </div>
-                            </Form>
-                        </Modal>
-                    </>
-                )
-            }
-
+                                        Cancel
+                                    </Button>
+                                </Form.Item>
+                                <Form.Item name="submit">
+                                    <Button
+                                        type="primary"
+                                        htmlType="submit">
+                                        Submit
+                                    </Button>
+                                </Form.Item>
+                            </div>
+                        </Form>
+                    </Modal>
+                </>
+            )}
         </>
     );
 };
--- frontend/app/src/components/TenantDashBoard.tsx
+++ frontend/app/src/components/TenantDashBoard.tsx
@@ -9,146 +9,179 @@ import { CardComponent } from "./reusableComponents/cardComponent";
 
 export const TenantDashBoard = () => {
     const handleOpenLocker = () => {
-        console.log("Package added successfully");
-        // Add your logic for adding a package here
+        console.log("handle open locker");
+        // Add your logic for getting a package here
     };
 
     return (
         <div className="container">
             <h1 className="my-4">Admin Dashboard</h1>
-            <AlertComponent
-                message="Welcome to the Tenant Dashboard"
-                description="Sign Yo Lease. Pay Daddy Rent"
-                type="warning"
-            />
-            {/* Dashboard Statistics Cards */}
-            <h2 className="my-3">Quick Actions</h2>
-            <div className="d-flex gap-4 my-5 w-100 justify-content-between">
-                <CardComponent
-                    title="Open Complaint form"
-                    value={10}
-                    description="Something not working right or disturbing you? Let us know."
-                    hoverable={true}
-                    icon={<ToolOutlined className="icon" />}
-                    button={
-                        <Link to="/tenant/admin-view-and-edit-work-orders-and-complaints">
-                            <ButtonComponent
-                                title="View All"
-                                type="primary"
-                                onClick={() => {}}
-                            />
-                        </Link>
-                    }
-                />
-                <CardComponent
-                    title="Package info"
-                    value={10}
-                    description="You have a package, head to your locker and click the button to open it"
-                    hoverable={true}
-                    icon={<InboxOutlined className="icon" />}
-                    button={
-                        // this should be the modal stuff
-                        <ButtonComponent
-                            title="Open Locker"
-                            type="primary"
-                            onClick={() => {
-                                console.log("modal should open");
-                                handleOpenLocker();
-                            }}
-                        />
-                    }
-                />
-                <CardComponent
-                    title="Guest Parking"
-                    value={10}
-                    description="Got a guest coming to visit? Make sure they have spots to park"
-                    hoverable={true}
-                    icon={<CarOutlined className="icon" />}
-                    button={
-                        // this should be the modal stuff
-                        <ButtonComponent
-                            title="Sign up guest"
-                            type="primary"
-                            onClick={() => {
-                                console.log("modal should open");
-                            }}
-                        />
-                    }
-                />
+            <div className="row">
+                <div className="col-sm-12">
+                    <AlertComponent
+                        message="Welcome to the Tenant Dashboard"
+                        description="Sign Yo Lease. Pay Daddy Rent"
+                        type="warning"
+                    />
+                </div>
             </div>
-            {/* Quick access cod */}
-            <h2 className="mb-3">Quick Access Documents Section</h2>
-            <div className="d-flex gap-4 my-5 w-100 justify-content-between">
-                <CardComponent
-                    title={"Lease"}
-                    description={"View or Resign your lease"}
-                    hoverable={true}
-                    button={
-                        <ButtonComponent
-                            title="Renew"
-                            type="primary"
-                            onClick={() => {
-                                console.log("open user lease to sign");
-                            }}
+
+            {/* Dashboard Statistics Cards */}
+            <h2 className="my-3 p-3">Quick Actions</h2>
+            <div className="row">
+                <div className="col-sm-12 col-md-4 p-sm-3">
+                    <CardComponent
+                        title="Open Complaint form"
+                        value={10}
+                        description="Something not working right or disturbing you? Let us know."
+                        hoverable={true}
+                        icon={<ToolOutlined className="icon" />}
+                        button={
+                            <Link to="/tenant/admin-view-and-edit-work-orders-and-complaints">
+                                <ButtonComponent
+                                    title="View All"
+                                    type="primary"
+                                    onClick={() => {}}
+                                />
+                            </Link>
+                        }
+                    />
+                </div>
+                <div className="col-sm-12 col-md-4 p-sm-3">
+                    <CardComponent
+                        title="Package info"
+                        value={10}
+                        description="You have a package, head to your locker and click the button to open it"
+                        hoverable={true}
+                        icon={<InboxOutlined className="icon" />}
+                        button={
+                            <ModalComponent
+                                type="Smart Locker"
+                                buttonTitle="Open Locker"
+                                content="Open Locker"
+                                buttonType="primary"
+                                handleOkay={() => handleOpenLocker()}
+                            />
+                        }
+                    />
+                </div>
+
+                <div className="col-sm-12 col-md-4 p-sm-3">
+                    <CardComponent
+                        title="Guest Parking"
+                        value={10}
+                        description="Got a guest coming to visit? Make sure they have spots to park"
+                        hoverable={true}
+                        icon={<CarOutlined className="icon" />}
+                        button={
+                            <ModalComponent
+                                type="Guest Parking"
+                                buttonTitle="Add Guest"
+                                content="Add guest to be able to park in the complex"
+                                buttonType="primary"
+                                handleOkay={() => {}}
+                            />
+                        }
+                    />
+                </div>
+            </div>
+            <div className="row p-3">
+                {/* Quick access cod */}
+                <h2 className="my-3">Quick Access Documents Section</h2>
+
+                <div className="row">
+                    <div className="col-sm-12 col-md-4 p-sm-3">
+                        <CardComponent
+                            title={"Lease"}
+                            description={"View or Resign your lease"}
+                            hoverable={true}
+                            button={
+                                <ModalComponent
+                                    type="default"
+                                    buttonTitle="Work order"
+                                    content="Work order should go here"
+                                    buttonType="primary"
+                                    handleOkay={() => {}}
+                                />
+                            }
                         />
-                    }
-                />
-                <CardComponent
-                    title={"Work Order"}
-                    description={<Tag color="orange">In Progress</Tag>}
-                    hoverable={true}
-                    button={
-                        <ButtonComponent
-                            title="View Details"
-                            type="primary"
-                            onClick={() => {
-                                console.log("open work order");
-                            }}
+                    </div>
+
+                    <div className="col-sm-12 col-md-4 p-sm-3">
+                        <CardComponent
+                            title={"Work Order"}
+                            description={<Tag color="orange">In Progress</Tag>}
+                            hoverable={true}
+                            button={
+                                <ModalComponent
+                                    type="default"
+                                    buttonTitle="Work order"
+                                    content="Work order should go here"
+                                    buttonType="primary"
+                                    handleOkay={() => {}}
+                                />
+                            }
                         />
-                    }
-                />
-                <CardComponent
-                    title={"Complaint Received"}
-                    description={`Our office received your complaint and will investiage immediately. "From: onegreatuser@hotmail.com: there are loud techo raves every night, even m..."`}
-                    hoverable={true}
-                    button={
-                        <ButtonComponent
-                            title="View Details"
-                            type="primary"
-                            onClick={() => {
-                                console.log("open work order");
-                            }}
+                    </div>
+
+                    <div className="col-sm-12 col-md-4 p-sm-3">
+                        <CardComponent
+                            title={"Complaint Received"}
+                            description={`Our office received your complaint and will investiage immediately. "From: onegreatuser@hotmail.com: there are loud techo raves every night, even m..."`}
+                            hoverable={true}
+                            button={
+                                <ModalComponent
+                                    type="default"
+                                    buttonTitle="Complaint"
+                                    content="Complaint should go here"
+                                    buttonType="primary"
+                                    handleOkay={() => {}}
+                                />
+                            }
                         />
-                    }
-                />
-                <CardComponent
-                    title={"Work Order"}
-                    description={<Tag color="green">Done</Tag>}
-                    hoverable={true}
-                    button={
-                        <ButtonComponent
-                            title="View Details"
-                            type="primary"
-                            onClick={() => {
-                                console.log("open work order");
-                            }}
+                    </div>
+                </div>
+                <div className="row p-3">
+                    <div className="col-sm-12 col-md-4 p-sm-3">
+                        <CardComponent
+                            title={"Work Order"}
+                            description={<Tag color="green">Done</Tag>}
+                            hoverable={true}
+                            button={
+                                <ModalComponent
+                                    type="default"
+                                    buttonTitle="Work order"
+                                    content="Work order should go here"
+                                    buttonType="primary"
+                                    handleOkay={() => {}}
+                                />
+                            }
                         />
-                    }
-                />
-                <CardComponent
-                    title={"Work Order"}
-                    description={<Tag color="green">Done</Tag>}
-                    hoverable={true}
-                    button={
-                        <ButtonComponent
-                            title="View Details"
-                            type="primary"
-                            onClick={() => {
-                                console.log("open work order");
-                            }}
+                    </div>
+
+                    <div className="col-sm-12 col-md-4 p-sm-3">
+                        <CardComponent
+                            title={"Work Order"}
+                            description={<Tag color="green">Done</Tag>}
+                            hoverable={true}
+                            button={
+                                <ButtonComponent
+                                    title="View Details"
+                                    type="primary"
+                                    onClick={() => {
+                                        <ModalComponent
+                                            type="default"
+                                            buttonTitle="Work order"
+                                            content="Work order should go here"
+                                            buttonType="primary"
+                                            handleOkay={() => {}}
+                                        />;
+                                    }}
+                                />
+                            }
                         />
-                    }
-                />
+                    </div>
+                </div>
             </div>
         </div>
     );
