22e0a912a5fdeebb21bfad263d981fb4e8c5e2fa
Diss <65290988+Dissurender@users.noreply.github.com>
2025-03-10 23:05:27 -0500

    remove demo tenant

--- backend/internal/db/schema/tenants.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE tenants (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL,
-    created_at TIMESTAMPTZ DEFAULT timezone('UTC', now())  -- I am making UTC our default. Do this for all models.
-);
