6f322a3ed0a17a4f698082b6a25b0fbe384b14af
schraderbachar <jj.schraderbachar@icloud.com>
2025-03-11 18:09:55 -0500

    fixed css and added modal component stuff

--- frontend/app/src/components/reusableComponents/CardComponent.tsx
+++ frontend/app/src/components/reusableComponents/CardComponent.tsx
@@ -26,13 +26,12 @@ export const CardComponent = (props: CardComponentProps) => {
         <>
             <Card
                 //TODO: JJ make these styles better
+                title={props.title}
                 hoverable={props.hoverable}
                 className="h-100 text-center"
                 style={{ minHeight: "280px", minWidth: "280px" }}>
-                <div className="d-flex flex-column align-items-center">
-                    {props.icon}
-
-                    <h4 className="mb-3">{props.title}</h4>
+                <div className="flex flex-column align-items-center">
+                    <span>{props.icon}</span>
 
                     <p className="text-muted">{props.description}</p>
 
