d5b3c6ef0e9f4aae10d0e705b6930af093544498
DailyDisco <actual.mensch0r@icloud.com>
2025-03-12 13:41:45 -0500

    Push updated parking_permits.sql

--- backend/internal/db/queries/parking_permits.sql
+++ backend/internal/db/queries/parking_permits.sql
@@ -1,8 +1,3 @@
--- Prepend each action with a name, the name can be the same as the action
--- EX: --name: get_parking_permits
-
--- name of table
-
 -- name: CreateParkingPermit :one
 INSERT INTO parking_permits (
     id,
@@ -10,29 +5,27 @@ INSERT INTO parking_permits (
     created_at, 
     updated_at, 
     expires_at
-    )
-VALUES(
+)
+VALUES (
     $1,
     $2,
     $3,
     $4,
     $5
-    )
+)
 RETURNING *;
 
 -- name: GetParkingPermit :one
 SELECT id, user_id, created_at, updated_at, expires_at
 FROM parking_permits
-WHERE id = $1;
+WHERE id = $1
 LIMIT 1;
 
 -- name: GetParkingPermits :many
 SELECT *
 FROM parking_permits
-ORDER BY create_at DESC;
--- LIMIT $1 OFFSET $2;
-
-
+ORDER BY created_at DESC
+LIMIT $1 OFFSET $2;
 
 -- name: DeleteParkingPermit :exec
 DELETE FROM parking_permits
