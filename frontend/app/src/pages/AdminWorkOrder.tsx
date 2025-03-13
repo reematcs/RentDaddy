bd8605f97f99f53e66fd1cb74001bc0d72fd96da
reece-iriye <ririye01@gmail.com>
2025-03-12 22:08:53 -0500

    Added alerts

--- frontend/app/src/pages/AdminWorkOrder.tsx
+++ frontend/app/src/pages/AdminWorkOrder.tsx
@@ -5,15 +5,20 @@ import ModalComponent from "../components/ModalComponent";
 import TableComponent from "../components/reusableComponents/TableComponent";
 import ButtonComponent from "../components/reusableComponents/ButtonComponent";
 import type { ColumnsType } from "antd/es/table/interface";
+import AlertComponent from "../components/reusableComponents/AlertComponent";
 
 const AdminWorkOrder = () => {
-    const handleAddPackage = () => {
+    const handleAddWorkOrder = () => {
         console.log("Added package successfully.")
     };
 
-    const columnsLeases: ColumnsType<{ name: string; roomNumber: string; leaseStatus: string }> = [
-        { title: "Name", dataIndex: "name" },
-        { title: "Room Number", dataIndex: "roomNumber" },
+    const handleAddComplaint = () => {
+        console.log("Added complaint successfully.")
+    };
+
+    const workOrders: ColumnsType<{ roomNumber: string; name: string; leaseStatus: string }> = [
+        { title: "Name", dataIndex: "roomNumber" },
+        { title: "Room Number", dataIndex: "name" },
         {
             title: "Lease Status",
             dataIndex: "leaseStatus",
@@ -22,6 +27,38 @@ const AdminWorkOrder = () => {
             ),
         },
     ];
-}
+
+    const overdueServiceCount: number = 5;
+    const recentlyCreatedServiceCount: number = 6;
+    const recentlyCompletedServiceCount: number = 1;
+
+    return (
+        <div className="container">
+            <h1 className="mb-4">Work-Orders & Complaints</h1>
+
+            {/* Alerts headers */}
+            <div className="d-flex w-100 justify-content-between">
+                {
+                    overdueServiceCount > 0 ?
+                        <AlertComponent
+                            description={`${overdueServiceCount} overdue services`}
+                        /> : null
+                }
+                {
+                    recentlyCreatedServiceCount > 0 ?
+                        <AlertComponent
+                            description={`${recentlyCreatedServiceCount} services created in past 48 hours`}
+                        /> : null
+                }
+                {
+                    recentlyCompletedServiceCount > 0 ?
+                        <AlertComponent
+                            description={`${recentlyCompletedServiceCount} services completed in past 24 hours`}
+                        /> : null
+                }
+            </div>
+        </div>
+    )
+};
 
 export default AdminWorkOrder;
