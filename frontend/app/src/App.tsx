6b9c1fc166a67824bfe8cd6a38daf6c85149b3b9
schraderbachar <jj.schraderbachar@icloud.com>
2025-03-13 19:22:11 -0500

    removed button stuff

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,10 +1,6 @@
 import HeroBanner from "./components/HeroBanner";
 import HomePageFAQs from "./components/HomePageFAQs";
 import HomePageFeaturesComponent from "./components/HomePageFeaturesComponent";
-import ClerkAuthDemo from "./components/ClerkAuthDemo";
-import DemoTestingComponent from "./components/DemoTestingComponent";
-import ButtonComponent from "./components/reusableComponents/ButtonComponent";
-import { InfoCircleOutlined, LoadingOutlined } from "@ant-design/icons";
 
 function App() {
     return (
@@ -19,70 +15,6 @@ function App() {
 
             <div>
                 <HomePageFAQs />
-            </div>
-
-            {/* Button Component */}
-            <div>
-                <h1 className="text-center text-2xl font-bold">Button Component</h1>
-                <div className="flex flex-col gap-4 justify-content-center">
-                    <ButtonComponent
-                        title="Default"
-                        type="default"
-                    />
-                    <ButtonComponent
-                        title="Primary"
-                        type="primary"
-                    />
-                    <ButtonComponent
-                        title="Secondary"
-                        type="secondary"
-                    />
-
-                    {/* Tertiary / Accent */}
-                    <ButtonComponent
-                        title="Tertiary / Accent"
-                        type="warning"
-                    />
-
-                    {/* Danger / Cancel */}
-                    <ButtonComponent
-                        title="Danger / Cancel"
-                        type="danger"
-                    />
-
-                    {/* Info */}
-                    <ButtonComponent
-                        title="Info"
-                        type="info"
-                        icon={<InfoCircleOutlined />}
-                    />
-
-                    {/* Loading */}
-                    <ButtonComponent
-                        title="Loading"
-                        type="loading"
-                        icon={<LoadingOutlined />}
-                    />
-
-                    {/* Disabled */}
-                    <ButtonComponent
-                        title="Disabled"
-                        type="disabled"
-                        disabled={true}
-                    />
-                </div>
-                {/* Different Sizes */}
-                <div className="flex flex-row gap-4 justify-content-center mt-4">
-                    <ButtonComponent
-                        title="Default"
-                        type="primary"
-                    />
-                    <ButtonComponent
-                        title="Large"
-                        type="primary"
-                        size="large"
-                    />
-                </div>
             </div>
         </>
     );
