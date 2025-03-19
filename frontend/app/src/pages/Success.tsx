634deccb965d18a17e89bd7fd10cef0d66f1f6d8
Reem Mokhtar <reem.mokhtar@gmail.com>
2025-03-19 17:24:18 +0300

    attempted to fix 400 response. rentdaddy-backend   | 2025/03/19 14:07:19 DOCUMENSO DEBUG: Sending document with URL: http://app.documenso.com/api/v1/documents/112596/send rentdaddy-backend   | 2025/03/19 14:07:20 DOCUMENSO DEBUG: Send document response status: 200 rentdaddy-backend   | 2025/03/19 14:07:20 DOCUMENSO DEBUG: Document 112596 successfully created and sent rentdaddy-backend   | 2025/03/19 14:07:20 DOCUMENSO DEBUG: Verifying document existence: http://app.documenso.com/api/v1/documents/112596 rentdaddy-backend   | 2025/03/19 14:07:20 DOCUMENSO DEBUG: Document verification status: 200 rentdaddy-backend   | 2025/03/19 14:07:20 DOCUMENSO DEBUG: Document 112596 exists in Documenso rentdaddy-backend   | 2025/03/19 14:07:20 "POST http://localhost:8080/admin/tenants/leases/upload-with-signers HTTP/1.1" from 192.168.65.1:45775 - 400 207B in 2.50969191

---
+++ frontend/app/src/pages/Success.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router";
+
+const ErrorNotFound = () => {
+    return (
+        <div className="d-flex justify-content-center align-items-center vh-100">
+            <div className="text-center mb-4">
+                <img
+                    src="/sad_logo.png"
+                    className="mb-4"
+                />
+                <h1 className="mb-4">Hooooray! Daddy can pay rent!</h1>
+                <Link
+                    to="/"
+                    className="mb-4">
+                    Go back to Home
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default ErrorNotFound;
