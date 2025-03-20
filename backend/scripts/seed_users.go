db25b02bbabedbb65cd1b42490dcb856debbf082
hector3211 <hectororopesa5@gmail.com>
2025-03-20 16:18:09 -0400

    first commit

---
+++ backend/scripts/seed_users.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/clerk/clerk-sdk-go/v2"
+	"github.com/clerk/clerk-sdk-go/v2/user"
+	"github.com/go-faker/faker/v4"
+)
+
+type Role string
+
+const (
+	RoleTenant Role = "tenant"
+	RoleAdmin  Role = "admin"
+)
+
+type ClerkUserPublicMetaData struct {
+	DbId       int32 `json:"db_id"`
+	Role       Role  `json:"role"`
+	UnitNumber int   `json:"unit_number"`
+	// Admin(clerk_id) inviting tenant
+	ManagementId string `json:"management_id"`
+}
+
+type ClerkUserEntry struct {
+	EmailAddresses []string        `json:"email_addresses"`
+	FirstName      string          `json:"first_name"`
+	LastName       string          `json:"last_name"`
+	PublicMetaData json.RawMessage `json:"public_metadata"`
+}
+
+func main() {
+	clerkSecretKey := os.Getenv("CLERK_SECRET_KEY")
+	if clerkSecretKey == "" {
+		log.Fatal("[SEED_USERS] CLERK_SECRET_KEY env required")
+		return
+	}
+
+	clerk.SetKey(clerkSecretKey)
+	ctx := context.Background()
+	userCount := 10
+
+	for i := 0; i < userCount; i++ {
+		if err := createUser(ctx); err != nil {
+			log.Printf("[SEED_USERS] Error seeding user %d: %v", i+1, err)
+		}
+	}
+}
+
+func createUser(ctx context.Context) error {
+	randomUnitNumbers, err := faker.RandomInt(101, 999)
+	if err != nil {
+		return err
+	}
+
+	unitNumber := 101
+	if len(randomUnitNumbers) > 0 {
+		unitNumber = randomUnitNumbers[0]
+	}
+
+	userMetadata := ClerkUserPublicMetaData{
+		DbId:         0,
+		Role:         RoleTenant,
+		UnitNumber:   unitNumber,
+		ManagementId: "",
+	}
+	metadataBytes, err := json.Marshal(userMetadata)
+	if err != nil {
+		return err
+	}
+	metadataRaw := json.RawMessage(metadataBytes)
+
+	userEntry := ClerkUserEntry{
+		EmailAddresses: []string{faker.Email()},
+		FirstName:      faker.FirstName(),
+		LastName:       faker.LastName(),
+		PublicMetaData: metadataRaw,
+	}
+
+	_, err = user.Create(ctx, &user.CreateParams{
+		EmailAddresses: &userEntry.EmailAddresses,
+		FirstName:      &userEntry.FirstName,
+		LastName:       &userEntry.LastName,
+		PublicMetadata: &userEntry.PublicMetaData,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
