1f534e546161905f6816140ac7a7db8586f59b4d
James Musselman <134752354+musselmandev@users.noreply.github.com>
2025-03-20 14:21:26 -0500

    dsd-138: Parking Handler (#87)
    * Progress on Handler
    
    * update
    
    * update: cleanup code
    
    * fix/update: clerk middleware and parking handler
    
    * update: working endpoints
    
    * added: grouping
    
    * added: delete
    
    * update: sever.go delete parking url
    
    * update: work_order routes
    
    * update: automatic deletion if expired
    
    * upate: work_order param search and 200 on empty
    
    * update: Workordertype
    
    * ready for merge
    
    ---------
    
    Co-authored-by: hector3211e <hectororopesa5@gmail.com>

--- backend/internal/db/queries/parking_permits.sql
+++ backend/internal/db/queries/parking_permits.sql
@@ -2,33 +2,39 @@
 INSERT INTO parking_permits (
     permit_number,
     created_by,
-    expires_at
+    expires_at,
+    updated_at
 )
 VALUES (
     $1,
     $2,
-    $3
+    $3,
+    now()
 )
 RETURNING *;
-
-
--- name: GetParkingPermit :one
-SELECT permit_number, created_by, updated_at, expires_at
-FROM parking_permits
-WHERE permit_number = $1
-LIMIT 1;
-
--- name: GetParkingPermits :many
-SELECT *
-FROM parking_permits
-ORDER BY created_by DESC
-LIMIT $1 OFFSET $2;
-
--- name: DeleteParkingPermit :exec
-DELETE FROM parking_permits
-WHERE id = $1;
 
 -- name: GetNumOfUserParkingPermits :one
 SELECT COUNT(*)
 FROM parking_permits
 WHERE created_by = $1;
+
+-- name: GetParkingPermit :one
+SELECT *
+FROM parking_permits
+WHERE id = $1
+LIMIT 1;
+
+-- name: GetTenantParkingPermits :many
+SELECT *
+FROM parking_permits
+WHERE created_by = $1;
+
+-- name: ListParkingPermits :many
+SELECT *
+FROM parking_permits
+ORDER BY created_by DESC;
+
+-- name: DeleteParkingPermit :exec
+DELETE FROM parking_permits
+WHERE id = $1;
+
