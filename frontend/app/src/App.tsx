da88fc28c2de613aed44051fe866f7cd6f00b677
James Musselman <jtmuss@pm.me>
2025-03-22 23:45:09 -0500

    FEAT: Add a chatbot with brains, and good looks

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -3,35 +3,37 @@ import HeroBanner from "./components/HeroBanner";
 import HomePageFAQs from "./components/HomePageFAQs";
 import HomePageFeaturesComponent from "./components/HomePageFeaturesComponent";
 import { useEffect } from "react";
+import MyChatBot from "./components/ChatBot";
 
 function App() {
-    const { getToken } = useAuth();
+  const { getToken } = useAuth();
 
-    useEffect(() => {
-        const fetchSession = async () => {
-            try {
-                const token = await getToken();
-                console.log(token);
-            } catch (error) {
-                console.error("Error fetching session token:", error);
-            }
-        };
+  useEffect(() => {
+    const fetchSession = async () => {
+      try {
+        const token = await getToken();
+        console.log(token);
+      } catch (error) {
+        console.error("Error fetching session token:", error);
+      }
+    };
 
-        fetchSession();
-    }, [getToken]);
+    fetchSession();
+  }, [getToken]);
 
-    return (
-        <>
-            <div className="container">
-                <HeroBanner />
-                <div className="my-2 flex-container">
-                    <HomePageFeaturesComponent />
-                </div>
+  return (
+    <>
+      <div className="container">
+        <HeroBanner />
+        <div className="my-2 flex-container">
+          <HomePageFeaturesComponent />
+        </div>
 
-                <HomePageFAQs />
-            </div>
-        </>
-    );
+        <HomePageFAQs />
+        <MyChatBot />
+      </div>
+    </>
+  );
 }
 
 export default App;
