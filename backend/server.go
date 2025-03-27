f9e84bd8b2c2e7bd3ab7f624c9f6ce683dbda967
Diego Espino <actual.mensch0r@icloud.com>
2025-03-26 17:12:31 -0700

    DSD-195: Integrate the Admin Dashboard

--- backend/server.go
+++ backend/server.go
@@ -84,6 +84,13 @@ func main() {
 	chatbotHandler := handlers.NewChatBotHandler(pool, queries)
 	complaintHandler := handlers.NewComplaintHandler(pool,queries)
 
+	// // Test routes - no auth required
+	// r.Post("/test/complaints", complaintHandler.CreateManyComplaintsForTestingHandler)
+
+	// r.Post("/test/work-orders", workOrderHandler.CreateManyWorkOrdersHandler)
+
+	// r.Post("/test/lockers", lockerHandler.CreateManyLockers)
+
 	// Application Routes
 	r.Group(func(r chi.Router) {
 		// Clerk middleware
@@ -186,6 +193,7 @@ func main() {
 		r.Post("/", chatbotHandler.ChatHandler)
 		r.Get("/", chatbotHandler.ChatGetHandler)
 	})
+
 	// Server config
 	port := os.Getenv("PORT")
 	server := &http.Server{
