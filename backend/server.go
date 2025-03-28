d5efa3a0d7e710bca6f249967d27ba60bd67d252
diss <65290988+Dissurender@users.noreply.github.com>
2025-03-27 19:15:06 -0500

    parking is pain

--- backend/server.go
+++ backend/server.go
@@ -217,7 +217,6 @@ func main() {
 			})
 			r.Route("/leases", func(r chi.Router) {
 				r.Get("/{user_id}/signing-url", func(w http.ResponseWriter, r *http.Request) {
-					leaseHandler := handlers.NewLeaseHandler(pool, queries)
 					leaseHandler.GetTenantLeaseStatusAndURLByUserID(w, r)
 				})
 			})
